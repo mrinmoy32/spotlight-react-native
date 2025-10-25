039cc860f2ea9c1b49393575b89831bbe4474ef8
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2025-10-26 01:19:42 +0530

    convex auth config and providers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,16 @@
-import { Stack } from "expo-router";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { ClerkProvider } from '@clerk/clerk-expo'
-import { tokenCache } from '@clerk/clerk-expo/token-cache'
-import { styles } from "@/styles/auth.styles";
 import InitialLayout from "@/components/initialLayout";
+import ClerkAndConvexProvider from "@/Providers/ClerkAndConvexProvider";
+import { styles } from "@/styles/auth.styles";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkAndConvexProvider>
       <SafeAreaProvider>
         <SafeAreaView style={styles.container}>
           <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
-    </ClerkProvider>
+    </ClerkAndConvexProvider>
   );
 }
